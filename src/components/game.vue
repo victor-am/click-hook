e44f1491e21a4954668da3db160a31aae6607601
Victor A.M <victor@victoryam.com.br>
2017-04-28 00:27:08 -0300

    Add basic income generation

---
+++ src/components/game.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="">
+    <p>You have ${{ currency }}</p>
+  </div>
+</template>
+
+<script>
+  const IncomeGenerationInterval = 1000
+
+  export default {
+    name: 'Game',
+
+    data () {
+      return {
+        currency: 0
+      }
+    },
+
+    mounted() {
+      this.scheduleIncomeGeneration()
+    },
+
+    methods: {
+      scheduleIncomeGeneration() {
+        window.setTimeout(this.incomeGenerate, IncomeGenerationInterval)
+      },
+
+      incomeGenerate() {
+        this.currency += this.income
+        this.scheduleIncomeGeneration()
+      }
+    },
+
+    computed: {
+      income() {
+        let base = 1
+        return base
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
